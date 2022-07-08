7ac6aa67aa86a9a508e7e27332633fd4b1bb6056
Parth Pandey <parthpandey2611@gmail.com>
2022-07-08 11:08:35 +0530

    Update views.py

--- News/views.py
+++ News/views.py
@@ -3,7 +3,7 @@ API_KEY = '2be644d283e44a0ba44b2a4a3a4b5aef'
 # Create your views here.
 import requests
 def index(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey=2be644d283e44a0ba44b2a4a3a4b5aef'
+    url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey=(2be644d283e44a0ba44b2a4a3a4b5aef)'
     data = requests.get(url)
     response = data.json()
     articles = response['articles']
@@ -11,7 +11,7 @@ def index(request):
     return render(request,'index.html',context)
 
 def category(request,name):
-    url = f'https://newsapi.org/v2/top-headlines?category={name}&apiKey=2be644d283e44a0ba44b2a4a3a4b5aef'
+    url = f'https://newsapi.org/v2/top-headlines?category={name}&apiKey=(2be644d283e44a0ba44b2a4a3a4b5aef)'
     data = requests.get(url)
     response = data.json()
     articles = response['articles']
@@ -20,7 +20,7 @@ def category(request,name):
 
 def search(request):
     search_term = request.GET['search']
-    url = f'https://newsapi.org/v2/everything?q={search_term}&apiKey=2be644d283e44a0ba44b2a4a3a4b5aef'
+    url = f'https://newsapi.org/v2/everything?q={search_term}&apiKey=(2be644d283e44a0ba44b2a4a3a4b5aef)'
     data = requests.get(url)
     response = data.json()
     articles = response['articles']
